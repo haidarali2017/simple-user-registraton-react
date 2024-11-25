d1841a7e3f22edc20d39ac306d7d0343c3e7a8d9
haidarali2017 <haidarcse2017@gmail.com>
2024-11-25 23:48:17 +0600

    simple user registration

---
+++ src/components/array.jsx
@@ -0,0 +1,23 @@
+// Filename - components/array.js
+
+// Javascript object named array
+// with 3 key-values
+const array = [
+  {
+    id: "1",
+    Name: "Haidar",
+    Age: "23",
+  },
+  {
+    id: "2",
+    Name: "Jewel",
+    Age: "22",
+  },
+  {
+    id: "3",
+    Name: "Saddam",
+    Age: "23",
+  },
+];
+
+export default array;
